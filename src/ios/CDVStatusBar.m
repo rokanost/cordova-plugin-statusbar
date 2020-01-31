0127d31d595e7b41189f5c446b452c9b67848cba
Rokas <rokanost@gmail.com>
2020-01-31 15:25:59 +1100

    Dark by default

--- src/ios/CDVStatusBar.m
+++ src/ios/CDVStatusBar.m
@@ -294,12 +294,7 @@ static const void *kStatusBarStyle = &kStatusBarStyle;
 
 - (void) styleDefault:(CDVInvokedUrlCommand*)command
 {
-    if (@available(iOS 13.0, *)) {
-        // TODO - Replace with UIStatusBarStyleDarkContent once Xcode 10 support is dropped
-        [self setStyleForStatusBar:3];
-    } else {
-        [self setStyleForStatusBar:UIStatusBarStyleDefault];
-    }
+    [self setStyleForStatusBar:UIStatusBarStyleDarkContent];
 }
 
 - (void) styleLightContent:(CDVInvokedUrlCommand*)command
